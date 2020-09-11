4948e1cab79046da46c935249fbbd5220b720906
Jing <60051579+Jing-321@users.noreply.github.com>
2020-09-11 15:05:01 -0700

    Update worksheet.rb

--- worksheet.rb
+++ worksheet.rb
@@ -1,34 +1,55 @@
+require 'csv'
+table = CSV.parse(File.read("rides.csv"), headers: true)
+
+ride_share = {}
+
+table.each do |row|
+  drive_id = row["DRIVER_ID"]
+  if !ride_share.has_key?(drive_id)
+    ride_share[drive_id] = []
+  end
+  ride_share[drive_id] << {date: row["DATE"], cost: row["COST"].to_i, rider_id: row["RIDER_ID"], rating: row["RATING"].to_i}
+end
 ########################################################
-# Step 1: Establish the layers
 
-# In this section of the file, as a series of comments,
-# create a list of the layers you identify.
-# Which layers are nested in each other?
-# Which layers of data "have" within it a different layer?
-# Which layers are "next" to each other?
-
-########################################################
-# Step 2: Assign a data structure to each layer
-
-# Copy your list from above, and in this section
-# determine what data structure each layer should have
-
-########################################################
-# Step 3: Make the data structure!
-
-# Setup the entire data structure:
-# based off of the notes you have above, create the
-# and manually write in data presented in rides.csv
-# You should be copying and pasting the literal data
-# into this data structure, such as "DR0004"
-# and "3rd Feb 2016" and "RD0022"
-
-########################################################
-# Step 4: Total Driver's Earnings and Number of Rides
-
-# Use an iteration blocks to print the following answers:
 # - the number of rides each driver has given
+puts "Number of rides:"
+ride_share.each_pair { |id, rides| puts "Driver: #{id} ==> #{rides.length} rides"  }
+driver_earning = {}
+
 # - the total amount of money each driver has made
-# - the average rating for each driver
+puts "\nDrivers' earning:"
+ride_share.each_pair do |id, rides|
+  earning = 0
+  rides.each {|ride| earning += ride[:cost]}
+  puts "Driver: #{id} ==> $ #{earning}"
+  driver_earning[id] = earning
+end
+
 # - Which driver made the most money?
+max_earning = driver_earning.max_by {|id, earning| earning}
+puts "#{max_earning[0]} made the most. The earning is $#{max_earning[1]}."
+
+# - the average rating for each driver
+puts "\nDrivers' average rating:"
+driver_rate = {}
+ride_share.each_pair do |id, rides|
+  rate_sum = 0
+  rides.each {|ride| rate_sum += ride[:rating]}
+  rate_avg = '%.2f' %(rate_sum/rides.length.to_f)
+  puts "Driver: #{id} ==> #{rate_avg}"
+  driver_rate[id] = rate_avg
+end
+
 # - Which driver has the highest average rating?
+max_rate = driver_rate.max_by {|id, rate| rate}
+puts "#{max_rate[0]} has the highest rating #{max_rate[1]}."
+
+# - For each driver, on which day did they make the most money?
+driver_earning_date = {}
+ride_share.each_pair do |id, rides|
+  daily_earning = 0
+  rides.each {}
+  driver_earning_date[id] = {rides["DATE"] => rides.each {|ride| ride["COST"]}
+end
+p driver_earning_date
